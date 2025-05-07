8d5db3052479b7bb0dca6eca46ba6cdfb246e7f5
Matheus Altrao <matheusaltrao2@gmail.com>
2025-05-07 13:00:07 -0400

    ✨ feat(config): Adiciona suporte a TypeScript estrito e auto-importação de componentes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  typescript: {
+    strict: true,
+  },
+  components: true,
+  imports: {
+    autoImport: true,
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: false,
